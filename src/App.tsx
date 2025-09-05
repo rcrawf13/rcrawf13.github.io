30adf70e5f2af911652e77429d28d69a96dd6f92
rcrawf13 <richmondcrawford44@gmail.com>
2025-09-05 13:03:46 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <section className='splash'>
+
+        <div className='frame'>
+          <div className="text">
+            <p>FullStack</p>
+            <p>Information Technology</p>
+          </div>
+        </div>
+      </section>
+      
+      <section className='info'>
+        <ul>
+          <li>About Me</li>
+          <li>Resume</li>
+          <li>Contact</li>
+        </ul>
+      </section>
+    </>
+  )
+}
+
+export default App
